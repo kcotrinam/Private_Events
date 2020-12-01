a332d06de4a5b911655b27b3aad7d4572f8a640f
kevin Cotrina <kcotrinam92@gmail.com>
2020-12-01 16:57:16 -0500

    Add attendees list

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,9 @@
 class Invitation < ApplicationRecord
   belongs_to :attendee, class_name: 'User', foreign_key: 'attendee_id'
   belongs_to :attended_event, class_name: 'Event', foreign_key: 'attended_event_id'
+
+  validates :attendee_id, uniqueness: { scope: :attended_event_id}
+  
+  
 end
+
